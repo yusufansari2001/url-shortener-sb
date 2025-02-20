7cce3ec8ad161f01f31b819e3b4c1f7566f1bec3
yusufansari2001 <yousuf89786@gmail.com>
2025-02-20 23:59:49 +0530

    Day 2 @20 Feb 2025

---
+++ src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package com.url.shortener.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private jwtUtils jwtProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            //get jwt from header
+            String jwt = jwtProvider.getJwtFromHeader(request);
+            //validate token
+            //if valid load the user and set in authentication context
+            if(jwt != null && jwtProvider.validateToken(jwt)) {
+                String userName = jwtProvider.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+                if(userDetails != null) {
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/url/shortener/services/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package com.url.shortener.services;
+
+import com.url.shortener.models.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionId = 1L;
+
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String userName, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return new UserDetailsImpl(
+              user.getId(),
+              user.getUserName(),
+              user.getEmail(),
+              user.getPassword(),
+                Collections.singletonList(authority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
